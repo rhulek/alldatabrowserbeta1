80eaaa071745d453b7b86b8c79c28062f729b458
Jiri Kalina <kalina@iba.muni.cz>
2014-03-16 22:41:00 +0100

    b2

--- R/ts.R
+++ R/ts.R
@@ -349,9 +349,6 @@ ts<-function(records,centralValueType="median",whiskerValueType="5_95",transform
     if (max(c(hole,dateTime[-1]-dateTime[-length(records[[i]]$values)]))>hole|(length(dateTime)<3)) { # Hole added to the vector to avoid problems with vector of length 1.
       series<-NA
     } else {
-      
-      return(list(valu,dateTime))
-      
       curve<-data.frame(as.Date(as.numeric(genplot(valu,dateTime,n=20,distr="lnorm",plot=FALSE)$belt[1,]),origin="1970-01-01"),
                         as.numeric(genplot(valu,dateTime,n=20,distr="lnorm",plot=FALSE)$line[1,]),
                         as.numeric(genplot(valu,dateTime,n=20,distr="lnorm",plot=FALSE)$lower[1,]),
@@ -384,6 +381,8 @@ ts<-function(records,centralValueType="median",whiskerValueType="5_95",transform
     if (max(c(hole,dateTime[-1]-dateTime[-length(records[[i]]$values)]))<=hole) {
       parameterNames<-c("slope",
                         "intercept")
+      
+      return(list(valu,dateTime))
       
       parameterValues<-c(genplot(valu,dateTime,n=20,distr="lnorm",plot=FALSE)$slope,
                          genplot(valu,dateTime,n=20,distr="lnorm",plot=FALSE)$intercept)
